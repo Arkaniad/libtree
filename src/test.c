185d82e5e97f792089875b06e9c675ae2ab5d989
Tanner Danzey <arkaniad@gmail.com>
2012-09-09 16:28:24 -0500

    Basic test structure

--- src/test.c
+++ src/test.c
@@ -5,7 +5,18 @@
 
 // Test libtree stuff
 int main(int argc, const char *argv[]){
+  int tests = 1;
+  int success = 0;
   printf(">>>> Starting libtree tests\n");
+  if(test_tree()){
+    success++;
+    printf(">>> [%d/%d] test_tree() SUCCESS\n", success, tests);
+  } else {
+    printf(">>> [%d/%d] test_tree() FAIL\n", success, tests);
+  }
+}
+
+int test_tree(){
   btree *tree;
   btree_node *root;
   
@@ -13,6 +24,10 @@ int main(int argc, const char *argv[]){
   root = btree_node_newnum(5);
   
   btree_set_root(tree, root);
+  
+  if(tree->root->num == 5){
+    return 1;
+  } else {
+    return 0;
+  }
 }
-
-
